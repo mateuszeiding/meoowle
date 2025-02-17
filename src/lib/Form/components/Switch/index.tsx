9b2dcb57c33da81a2582ca7a35046fbcc8351f48
MATEUSZ EIDING <eidingmateusz@gmail.com>
2025-02-17 14:32:54 +0100

    Fix switch 3d styles

--- src/lib/Form/components/Switch/index.tsx
+++ src/lib/Form/components/Switch/index.tsx
@@ -10,17 +10,17 @@ type Props = InputHTMLAttributes<HTMLInputElement> & {
 export default function (props: Props) {
 	const ref = useRef<HTMLInputElement | null>(null);
 	return (
-		<div
-			className={cls("form-switch", props.thirdDimension && "third-dimension")}
-		>
-			<input
-				{...props}
-				aria-checked={ref.current?.checked}
-				ref={ref}
-				type="checkbox"
-				role="switch"
-				className={cls("form-check-input", `theme-${props.theme ?? "default"}`)}
-			/>
-		</div>
+		<input
+			{...props}
+			aria-checked={ref.current?.checked}
+			ref={ref}
+			type="checkbox"
+			role="switch"
+			className={cls(
+				"form-check-input",
+				props.thirdDimension && "third-dimension",
+				`theme-${props.theme ?? "default"}`,
+			)}
+		/>
 	);
 }
